cf4a95e1dd3356fe7555fafd2cb6c570335ef927
Robert Hager <RobertHager@gmx.de>
2020-04-15 23:27:04 +0200

    webhook test2

--- backend/mySqlFlask/functions.py
+++ backend/mySqlFlask/functions.py
@@ -44,7 +44,7 @@ def user():
 def not_found(error=None):
     message = {
         'status': 404,
-        'message': 'watch your mouth,  ' + request.url,
+        'message': 'watch your mouths,  ' + request.url,
     }
     resp = jsonify(message)
     resp.status_code = 404
