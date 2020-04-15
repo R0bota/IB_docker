6d6449602f1b9749010ba6fb03c3275a38dda9e8
Robert Hager <RobertHager@gmx.de>
2020-04-15 23:39:46 +0200

    test3

--- backend/mySqlFlask/functions.py
+++ backend/mySqlFlask/functions.py
@@ -44,7 +44,7 @@ def user():
 def not_found(error=None):
     message = {
         'status': 404,
-        'message': 'watch your mouths,  ' + request.url,
+        'message': 'watch your mouthss,  ' + request.url,
     }
     resp = jsonify(message)
     resp.status_code = 404
